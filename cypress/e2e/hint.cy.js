e9be4e9e5aed220dba79e17fafd6ba1d624805bc
Alexander Maximov <maximova@odysseyvc.com>
2023-02-04 22:27:32 +0100

    Add hint, modes tests

---
+++ cypress/e2e/hint.cy.js
@@ -0,0 +1,16 @@
+describe('Hint', () => {
+  const preFilledCellsNumber = 45;
+
+  it('fills each empty cell', () => {
+    cy.visit('/');
+    cy.get('.game__cell--filled').should('have.length', preFilledCellsNumber);
+    cy.get('.game__cell')
+      .not('.game__cell--filled')
+      .each(($cell) => {
+        cy.wrap($cell).click();
+        cy.get('.status__action-hint').click();
+      });
+
+    cy.contains('.overlay__text', 'You solved it').should('be.visible');
+  });
+});
---
+++ cypress/e2e/modes.cy.js
@@ -0,0 +1,36 @@
+describe('Play mode - shows a different number of empty cells', () => {
+  const preFilledCellsNumberEasy = 45;
+  const preFilledCellsNumberMedium = 40;
+  const preFilledCellsNumberHard = 30;
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('shows Easy mode by default', () => {
+    cy.get('.status__difficulty-select').should('have.value', 'Easy');
+  });
+
+  it('shows 45 empty cells for Easy mode', () => {
+    cy.get('.game__cell--filled').should(
+      'have.length',
+      preFilledCellsNumberEasy
+    );
+  });
+
+  it('shows 40 empty cells for Medium mode', () => {
+    cy.get('.status__difficulty-select').select('Medium');
+    cy.get('.game__cell--filled').should(
+      'have.length',
+      preFilledCellsNumberMedium
+    );
+  });
+
+  it('shows 40 empty cells for Hard mode', () => {
+    cy.get('.status__difficulty-select').select('Hard');
+    cy.get('.game__cell--filled').should(
+      'have.length',
+      preFilledCellsNumberHard
+    );
+  });
+});
