4e7694d1e07875df1e0bbd0d93102ec9f2d9bd29
Alexander Maximov <maximova@odysseyvc.com>
2023-02-08 22:13:04 +0100

    Timer tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 const cypressJsonConfig = {
   baseUrl: 'http://localhost:3000',
   watchForFileChanges: false,
+  viewportHeight: 800,
 };
 
 export default defineConfig({
