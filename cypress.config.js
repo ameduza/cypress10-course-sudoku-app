a207a0063f9546fba72285d591efed99c772e2f3
Alexander Maximov <maximova@odysseyvc.com>
2023-02-15 23:03:42 +0100

    Setup component tests

--- cypress.config.js
+++ cypress.config.js
@@ -13,4 +13,11 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack',
+    },
+  },
 });
